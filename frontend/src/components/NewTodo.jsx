6a3b6e9734909a2cc9ed3d94c0688f534d3c3735
itric <itric46@gmail.com>
2024-04-12 16:32:53 +0545

    file structure changed

--- frontend/src/components/NewTodo.jsx
+++ frontend/src/components/NewTodo.jsx
@@ -1,39 +1,65 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../redux/todo/todoSlice";
 
+const initialState = {
+  id: "",
+  name: "",
+  completed: false,
+  importantUrgentCategory: "None",
+  todoCategory: "None",
+  localPriorityText: "None",
+  localPriorityNum: 0,
+  globalPriorityText: "None",
+  globalPriorityNum: 0,
+};
+
+const ACTIONS = {
+  CHANGE_INPUT: "CHANGE_INPUT",
+};
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.CHANGE_INPUT:
+      return {
+        ...state,
+        [action.payload.name]: action.payload.value,
+      };
+    default:
+      return state;
+  }
+};
+
 function NewTodo() {
   const dispatch = useDispatch();
-  const [newTodo, setNewTodo] = useState({
-    id: crypto.randomUUID(),
-    name: "",
-    completed: false,
-    importantUrgentCatagory: "None",
-    todoCatagory: "None",
-    localPriorityText: "None",
-    localPriorityNum: 0,
-    globalPriorityText: "None",
-    globalPriorityNum: 0,
-  });
+  const [state, dispatcher] = useReducer(todoReducer, initialState);
+  const [newTodo, setNewTodo] = useState({});
   const [advanceMode, setAdvanceMode] = useState(false);
-  const handleChange = (e) => {
-    if (e.target.id === "todo_name") {
-      setNewTodo({
-        ...newTodo,
-        name: e.target.value,
-      });
-    }
-  };
+  // const handleChange = (e) => {
+  //   if (e.target.id === "todo_name") {
+  //     setNewTodo({
+  //       ...newTodo,
+  //       name: e.target.value,
+  //     });
+  //   }
+  // };
 
   function createTodo() {
-    dispatch(addTodo({ ...newTodo }));
-    setNewTodo({
-      id: crypto.randomUUID(),
-      name: "",
-      completed: false,
-    });
+    dispatch(addTodo({ ...state }));
+    // setNewTodo({
+    //   id: crypto.randomUUID(),
+    //   name: "",
+    //   completed: false,
+    // });
   }
+
+  const handleChange = (e) => {
+    dispatcher({
+      type: "CHANGE_INPUT",
+      payload: { name: e.target.name, value: e.target.value },
+    });
+  };
 
   return (
     <>
@@ -42,6 +68,7 @@ function NewTodo() {
           type="text"
           placeholder="Enter task"
           id="todo_name"
+          name="name"
           value={newTodo.name}
           onChange={handleChange}
         />
@@ -49,8 +76,13 @@ function NewTodo() {
       </section>
       <br />
       <div className="flex">
-        <input type="text" placeholder="Enter Catagory" name="" id="" />
-        <button>Add Catagory</button>
+        <input
+          type="text"
+          placeholder="Enter Category"
+          name="todoCategory"
+          id=""
+        />
+        <button>Add Category</button>
         <select name="" id="">
           <option value="study">Study</option>
           <option value="Code">Code</option>
@@ -60,8 +92,8 @@ function NewTodo() {
       {advanceMode && (
         <div>
           <div>
-            <span>Important & Urgent Catagory</span>
-            <select name="" id="">
+            <span>Important & Urgent Category</span>
+            <select name="importantUrgentCategory" id="">
               <option value="None">None</option>
               <option value="importantUrgent">importantUrgent</option>
               <option value="NotimportantUrgent">
@@ -79,8 +111,8 @@ function NewTodo() {
             <span>Set priority</span>
             <div>
               <h6>Local priority</h6>
-              <input type="number" />
-              <select name="" id="">
+              <input type="number" name="localPriorityNum" />
+              <select name="localPriorityText" id="">
                 <option value="None">None</option>
                 <option value="Highest">Highest</option>
                 <option value="lowest">lowest</option>
@@ -88,8 +120,8 @@ function NewTodo() {
             </div>
             <div>
               <h6>Global priority</h6>
-              <input type="number" />
-              <select name="" id="">
+              <input type="number" name="globalPriorityNum" />
+              <select name="globalPriorityText" id="">
                 <option value="None">None</option>
                 <option value="Highest">Highest</option>
                 <option value="lowest">lowest</option>
